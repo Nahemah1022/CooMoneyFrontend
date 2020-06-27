e6cb0d62fa9da9908c8e2a478d88f1f68c4c79c2
Yin-Tung <a122774007@gmail.com>
2020-06-28 02:13:39 +0800

    [New] Member list tab

--- src/components/Club/Club.vue
+++ src/components/Club/Club.vue
@@ -20,19 +20,28 @@
       </div>
     </div>
     <div class="block">
-      <div class="tabs" :style="{ top: tabIndex===-1 ? '-36px' : '-40px' }">
+      <div class="tabs" :style="{ top: tabIndex===-1 ? '-32px' : '-36px' }">
         <div
           class="tab"
           v-for="(tab, index) in tabs"
           :key="index"
-          :style="{ height: tabIndex===index ? '40px' : '36px' }"
+          :class="{selected: tabIndex===index}"
           @click="tabIndex=index"
         >{{ tab.name }}</div>
       </div>
       <div class="space"></div>
-      <div class="intro">
+      <div v-if="tabIndex===0" class="intro">
         <div class="title">社群簡介</div>
         <div class="content">{{ club.intro }}</div>
+      </div>
+      <div v-if="tabIndex===2" class="member">
+        <div class="header">
+          <span>成員總數</span>
+          <span>{{ members.length + "人" }}</span>
+        </div>
+        <div class="memberBlock">
+          <MemberItem v-for="(member, index) in members" :key="index" :idx="index+1" :user="member"></MemberItem>
+        </div>
       </div>
     </div>
     <Footer :active="3"></Footer>
@@ -42,12 +51,14 @@
 <script>
 import Header from "@/components/common/Header.vue";
 import Footer from "@/components/common/Footer.vue";
+import MemberItem from "@/components/Club/MemberItem.vue";
 
 export default {
   name: "Club",
   components: {
     Header,
-    Footer
+    Footer,
+    MemberItem
   },
   data() {
     return {
@@ -60,6 +71,79 @@ export default {
         intro:
           "\t已算不清多少個日與夜，那身影始終懸於海岬之上，任風吹日曬雨淋；飽經風雨摧殘的臉龐上，歷經滄桑的雙眸鑲嵌其中，目光開闔之間，自有黯淡金光流瀉而出－－那是神性的光輝。\n\n\t但那身影卻好似永遠看不膩，始終懸於那處，遠眺著黑海盡頭的海天一色，從日出到日落，看盡潮起潮落。\n\n\t黎明破曉、曙光乍現，萬丈金光刺破闃黑的夜，鎏金的海映著黯金的瞳，純金的世界讓人想起了奧林帕斯的金碧輝煌，在那場天地變色的諸神戰役中，他毅然決然投身奧林帕斯陣營，劍指自己的手足－－泰坦神族。"
       },
+      members: [
+        { 
+          username: "陳尹曈", 
+          headshot: "https://cdn.icon-icons.com/icons2/1488/PNG/512/5347-github_102542.png", 
+          email: "a122774007@gmail.com", 
+          date: "2019/07/15", 
+          auth: 1
+        },
+        { 
+          username: "倪皓城", 
+          headshot: "https://cdn.icon-icons.com/icons2/1488/PNG/512/5347-github_102542.png", 
+          email: "zzzzzz@gmail.com", 
+          date: "2019/12/15", 
+          auth: 0
+        },
+        { 
+          username: "辛普森", 
+          headshot: "https://cdn.icon-icons.com/icons2/1488/PNG/512/5347-github_102542.png", 
+          email: "hinpson@gmail.com", 
+          date: "2019/11/16", 
+          auth: 1
+        },
+        { 
+          username: "陳力聖", 
+          headshot: "https://cdn.icon-icons.com/icons2/1488/PNG/512/5347-github_102542.png", 
+          email: "shang@gmail.com", 
+          date: "2019/10/26", 
+          auth: 0
+        },
+        { 
+          username: "陳明心", 
+          headshot: "https://cdn.icon-icons.com/icons2/1488/PNG/512/5347-github_102542.png", 
+          email: "travix@gmail.com", 
+          date: "2019/08/13", 
+          auth: 0
+        },
+        {
+          username: "阿拉哩", 
+          headshot: "https://cdn.icon-icons.com/icons2/1488/PNG/512/5347-github_102542.png", 
+          email: "alale@gmail.com", 
+          date: "2019/09/15", 
+          auth: 0
+        },
+        {
+          username: "阿拉哩", 
+          headshot: "https://cdn.icon-icons.com/icons2/1488/PNG/512/5347-github_102542.png", 
+          email: "alale@gmail.com", 
+          date: "2019/09/15", 
+          auth: 0
+        },
+        {
+          username: "阿拉哩", 
+          headshot: "https://cdn.icon-icons.com/icons2/1488/PNG/512/5347-github_102542.png", 
+          email: "alale@gmail.com", 
+          date: "2019/09/15", 
+          auth: 0
+        },
+        {
+          username: "阿拉哩", 
+          headshot: "https://cdn.icon-icons.com/icons2/1488/PNG/512/5347-github_102542.png", 
+          email: "alale@gmail.com", 
+          date: "2019/09/15", 
+          auth: 0
+        },
+        {
+          username: "阿拉哩", 
+          headshot: "https://cdn.icon-icons.com/icons2/1488/PNG/512/5347-github_102542.png", 
+          email: "alale@gmail.com", 
+          date: "2019/09/15", 
+          auth: 0
+        },
+
+      ],
       tabs: [{ name: "簡介" }, { name: "公告" }, { name: "成員" }],
       tabIndex: 0
     };
@@ -125,7 +209,7 @@ $tabHeight: 36px;
       align-items: flex-end;
       .tab {
         width: 33%;
-        height: $tabHeight;
+        height: 36px;
         background-color: #fff;
         box-shadow: 0 -5px 6px rgb(238, 238, 238) inset;
         border: 1px solid #e9eaed;
@@ -137,6 +221,12 @@ $tabHeight: 36px;
         font-size: 22px;
         font-weight: 700;
         color: #00c5b8;
+        &.selected {
+          height: 40px;
+          background-color: #00c5b8;
+          color: #fff;
+          box-shadow: none;
+        }
       }
     }
     .space {
@@ -148,11 +238,32 @@ $tabHeight: 36px;
       color: #8d8d8d;
       .title {
         font-size: 26px;
+        font-weight: 700;
         margin: 2vh 0;
       }
       .content {
         white-space: pre-wrap;
         font-size: 14px;
+      }
+    }
+    .member {
+      height: 70vh;
+      .header {
+        height: 60px;
+        display: flex;
+        justify-content: space-between;
+        align-items: flex-end;
+        border-bottom: 1px solid #e4e4e4;
+        span {
+          margin: 5px 10px;
+          font-weight: 700;
+          color: #00c5b8;
+        }
+      }
+      .memberBlock {
+        height: 60vh;
+        overflow-x: hidden;
+        overflow-y: scroll;
       }
     }
   }
