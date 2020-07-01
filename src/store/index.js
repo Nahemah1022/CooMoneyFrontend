e73c93d65e80dfbe3bca6e72686217d81d5bf605
Ohlamesaint <mkop9456@gmail.com>
2020-07-01 19:44:28 +0800

    [VUEX] persisted state

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import actions from "./action";
+import createPersistedState from 'vuex-persistedstate'
+import * as Cookies from 'js-cookie'
+
 
 Vue.use(Vuex);
 
@@ -28,4 +31,13 @@ export default new Vuex.Store({
   actions,
   mutations: {},
   modules: {},
+  plugins: [
+    createPersistedState({
+      storage: {
+        getItem: (key) => Cookies.get(key),
+        setItem: (key, value) => Cookies.set(key, value, { expires: 3, secure: true }),
+        removeItem: (key) => Cookies.remove(key)
+      }
+    })
+  ]
 });
